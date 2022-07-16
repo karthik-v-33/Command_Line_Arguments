f81ee095c918bd58aae69fdaf491b345838fc811
Karthik <105358710+karthik-v-33@users.noreply.github.com>
2022-07-16 17:06:50 +0530

    Add files via upload

---
+++ Command_Line_Arguments.java
@@ -0,0 +1,11 @@
+package com.GitHub_Updates;
+
+public class Command_Line_Arguments {
+	//Command Line Arguments in Java
+	public static void main(String args[]) {
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
